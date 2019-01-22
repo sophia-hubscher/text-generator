7d6fde990bbc1d977dadcad7183caba76b003cd5
Sophia Hubscher <33038334+sophia-hubscher@users.noreply.github.com>
2019-01-22 09:57:38 -0500

    New window size

--- src/TextGenerator.java
+++ src/TextGenerator.java
@@ -2,14 +2,14 @@
  * TextGenerator
  *
  * @author Sophia Hübscher
- * @version 2.3
+ * @version 2.4
  */
 public class TextGenerator
 {
     public static void main(String[] args)
     {
         //the JFrame size:
-        int windowWidth = 1210;
+        int windowWidth = 765;
         int windowHeight = 780;
 
         //make Frame, handing over Rabbit object to it
